cbf2529986012bbbafdd7a172b9fb2cead27b029
SANTHOSH M <148633650+Santhosh9600@users.noreply.github.com>
2026-06-05 17:27:03 +0530

    commit
    Removed 'All Rights Reserved' from footer text.

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -11,7 +11,7 @@ const Contact=()=>{
     </div>
 
       <div className="footer-contact">
-        Blood Sharing Portal All Rights Reserved
+        Blood Sharing Portal
       </div>
       </>
   );
